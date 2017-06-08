9ada5c7c152c7cb2d03037d009651c924e45eb06
Marco Mancuso <marco.mancuso@alfresco.com>
2017-06-08 14:12:31 +0100

    cookstyle fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@ default['aps-core']['version'] = '1.6.0'
 default['aps-core']['nexus']['username'] = 'your-nexus-username'
 default['aps-core']['nexus']['password'] = 'your-nexus-password'
 
-#db settings for activiti properties
+# db settings for activiti properties
 default['aps-core']['db']['engine'] = 'mysql'
 default['aps-core']['db']['host'] = 'locahost'
 
