c7f9d4b0033e16a7d9d77eca3cffed8075cd03a5
Martin Whittington <martin.whittington@alfresco.com>
2017-09-15 16:49:26 +0100

    Admin app work

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,9 +11,16 @@ default['aps-core']['db']['host'] = '127.0.0.1'
 default['aps-core']['mysql_driver']['version'] = '5.1.32'
 mysql_driver_version = node['aps-core']['mysql_driver']['version']
 default['aps-core']['mysql_driver']['url'] = lazy { "http://central.maven.org/maven2/mysql/mysql-connector-java/#{mysql_driver_version}/mysql-connector-java-#{mysql_driver_version}.jar" }
+
 # postgres driver
 default['aps-core']['postgres_driver']['version'] = '42.1.1'
 postgres_driver_version = node['aps-core']['postgres_driver']['version']
 default['aps-core']['postgres_driver']['url'] = lazy { "https://jdbc.postgresql.org/download/postgresql-#{postgres_driver_version}.jar" }
 default['aps-core']['appserver']['tomcat_home'] = '/usr/share/tomcat'
 default['aps-core']['war_file_paths_to_override'] = %w()
+
+# admin app controls
+default['aps-core']['admin_app']['install'] = false
+default['aps-core']['admin_app']['version'] = '1.6.4'
+default['aps-core']['admin_db']['engine'] = 'mysql'
+default['aps-core']['admin_db']['host'] = '127.0.0.1'
--- recipes/default.rb
+++ recipes/default.rb
@@ -11,11 +11,20 @@ include_recipe 'aps-core::_download_artifacts'
 tomcat_home = node['aps-core']['appserver']['tomcat_home']
 
 template "#{tomcat_home}/lib/activiti-app.properties" do
-  source 'activiti-app.properties.erb'
+  source 'activiti-properties.erb'
   owner 'tomcat'
   group 'tomcat'
   mode 00740
   variables properties: node['aps-core']['activiti-app-properties']
+end
+
+template "#{tomcat_home}/lib/activiti-admin.properties" do
+  source 'activiti-properties.erb'
+  owner 'tomcat'
+  group 'tomcat'
+  mode 00740
+  variables properties: node['aps-core']['activiti-admin-properties']
+  only_if { node['aps-core']['admin_app']['install'] }
 end
 
 service "#{node['appserver']['installname']}-#{node['tomcat']['service']}" do
